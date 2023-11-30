72b10f0f7f15f3c68de7926d6a278c24c730a603
Gmongtor <gmongtor@myuax.com>
2023-11-30 11:52:19 +0100

    commit estructuras

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -2,3 +2,12 @@
 #include <string>
 #include <cstdio>
 using namespace std;
+struct ColorConsole {
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto fg_white = "\033[47m";
+};
+struct ConsoleBox {
+    void new_text() {/*...*/};
+    void set_text (const string &text){cout << text << endl;}
+};
+
